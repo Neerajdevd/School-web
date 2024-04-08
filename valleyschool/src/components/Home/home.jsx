a1c592b618c3f7eec19b8be05588972dc38f47d4
Neerajdevd <devdarjun03@gmail.com>
2024-04-08 04:31:22 -0700

    ui navbar and crousel

---
+++ valleyschool/src/components/Home/home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Navbar/navbar"
+import Crousell from "../crousel/crousell"
+
+const Home =()=>{
+    return(
+        <div>
+       <Navbar/>
+       <Crousell/>
+
+        </div>
+       
+    )
+}
+
+export default Home
